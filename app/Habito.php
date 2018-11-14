54ad76c12d0269ecac83154be57f829b686228bf
Paularapkievicz <143446@upf.br>
2018-11-14 20:15:12 -0200

    Primeiro commit do projeto

---
+++ app/Habito.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Habito extends Model
+{
+    protected $fillable = ['nome', 'descricao','tp_habito', 'dt_inicio_ctrl', 'objetivo'];
+
+
+    public function historico(){
+        return $this->hasMany('App\Historico');
+    }
+
+}
